3778fc3688722403fed73c39dbc0525cf2729cc4
xmeiko7 <221250155@smail.nju.edu.cn>
2024-08-13 18:27:39 +0800

    nothing

--- src/main/java/org/example/cloud_native_project/GreetController.java
+++ src/main/java/org/example/cloud_native_project/GreetController.java
@@ -1,33 +1,32 @@
 package org.example.cloud_native_project;
 
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Bucket4j;
-import io.github.bucket4j.Refill;
-import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class GreetController {
 
-    private final Bucket bucket;
-    public GreetController() {
-        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(100, Duration.ofSeconds(1)));
-        this.bucket = Bucket4j.builder()
-                .addLimit(limit)
-                .build();
-    }
 
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
+
+
+    public GreetController() {
+
+    }
     @GetMapping("/greet")
-    public ResponseEntity<Map<String, String>> hello() {
-        if (bucket.tryConsume(1)) {
+    public ResponseEntity<Map<String, String>>  limitedEndpoint() {
+        Long increment = redisTemplate.opsForValue().increment("global");
+        redisTemplate.expire("global",1, TimeUnit.SECONDS);
+        if (increment < 100) {
             return ResponseEntity.ok(Collections.singletonMap("msg", "hello"));
         } else {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
